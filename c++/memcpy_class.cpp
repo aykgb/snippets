0cd3f03b600e56cd1d71acfdd62ab098c3f2186f
wangchenglin <wangcl.cw1@gmail.com>
2019-10-10 08:01:10 +0800

    delete some files.
    Signed-off-by: wangchenglin <wangcl.cw1@gmail.com>

--- c++/memcpy_class.cpp
+++ c++/memcpy_class.cpp
@@ -11,12 +11,12 @@ class Base
 public:
 	Base() { cout << "It's Base's constructor!" << endl; }
 	~Base() { cout << "It's Base's destructor!" << endl; }
-	
+
 	void fun()
 	{
 		cout << "It's the Base's fun function." << endl;
 	}
-	
+
 private:
 	int index;
 	static int index;
@@ -25,9 +25,9 @@ private:
 int main()
 {
 	Base baseTst;
-	
+
 	char memtmp[sizeof(baseTst)];
 	memcpy(memtmp, &baseTst, sizeof(baseTst));
-	
+
 	return 0;
 }
