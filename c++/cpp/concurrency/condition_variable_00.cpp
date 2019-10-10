a5e8162913e65dc4f27c7482455ae5d67c43fa8e
wangchenglin <wangcl.cw1@gmail.com>
2019-10-10 09:03:11 +0800

    c++ std::thread shall verify joinable.
    Signed-off-by: wangchenglin <wangcl.cw1@gmail.com>

--- c++/cpp/concurrency/condition_variable_00.cpp
+++ c++/cpp/concurrency/condition_variable_00.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <thread>
+#include <chrono>
 #include <condition_variable>
+#include <iostream>
 #include <mutex>
+#include <thread>
+#include <vector>
 
 using namespace std;
 
@@ -10,40 +11,39 @@ std::mutex mtx;
 std::condition_variable cv;
 bool ready = false;
 
-void do_print_id(int id)
-{
-    std::unique_lock<std::mutex> lck(mtx);
-    std::cout << "function id: " << id
-              << "is waiting for lck ..." << std::endl;
+void do_print_id(int id) {
+  std::unique_lock<std::mutex> lck(mtx);
+  std::cout << "function id: " << id << "is waiting for lck ..." << std::endl;
 
-    while(!ready)
-        cv.wait(lck);
+  while (!ready) cv.wait(lck);
 
-    std::cout << "function id: " << id << " is finished." << std::endl;
+  std::cout << "function id: " << id << " is finished." << std::endl;
 }
 
-void go()
-{
-    std::unique_lock<std::mutex> lck(mtx);
-    ready = true;
-    cv.notify_all();
+void go() {
+  std::unique_lock<std::mutex> lck(mtx);
+  ready = true;
+  cv.notify_all();
 }
 
-int main()
-{
-    std::vector<std::thread> threads(10);
+int main() {
+  auto thread_nums = 5;
+  std::vector<std::thread> threads(thread_nums);
 
-    for(int i = 0; i < 10; ++i) {
-        threads[i] = std::thread(do_print_id, i);
-    }
+  for (int i = 0; i < thread_nums; ++i) {
+    threads[i] = std::thread(do_print_id, i);
+  }
 
-    std::cout << "10 threads ready to race ..." << std::endl;
+  std::cout << "thread_nums " << thread_nums << "threads ready to race ..."
+            << std::endl;
 
-    go();
+  go();
 
-    for(auto& th:threads)
-        th.join();
+  for (int i = 0; i < thread_nums; ++i) {
+      if (threads[i].joinable()) { // !must verify joinable condition.
+          threads[i].join();
+      }
+  }
 
-    return 0;
+  return 0;
 }
-
