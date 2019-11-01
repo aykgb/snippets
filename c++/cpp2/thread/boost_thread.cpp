210a60bc57a6960ac3027746757326f4948e2df2
wangchenglin <wangcl.cw1@gmail.com>
2019-11-01 15:36:15 +0800

    update
    Signed-off-by: wangchenglin <wangcl.cw1@gmail.com>

--- c++/cpp2/thread/boost_thread.cpp
+++ c++/cpp2/thread/boost_thread.cpp
@@ -3,39 +3,36 @@
 
 boost::mutex mutex;
 
-void threadA()
-{
-    for (int i = 10; i > 0; ) {
-        boost::mutex::scoped_lock lock(mutex);
-        {
-            fprintf(stderr, "Locked by A\n");
-            usleep(100000);
-            fprintf(stderr, "Unlocked by A\n");
-            i--;
-        }
+void threadA() {
+  for (int i = 10; i > 0;) {
+    boost::mutex::scoped_lock lock(mutex);
+    {
+      fprintf(stderr, "Locked by A\n");
+      usleep(100000);
+      fprintf(stderr, "Unlocked by A\n");
+      i--;
     }
+  }
 }
 
-void threadB()
-{
-    for (int i = 10; i > 0; ) {
-        boost::mutex::scoped_lock lock(mutex, boost::try_to_lock);
-        if (lock) {
-            fprintf(stderr, "Locked by B\n");
-            usleep(100000);
-            fprintf(stderr, "Unlocked by B\n");
-            i--;
-        } else {
-            fprintf(stderr, "Lock failed in B\n");
-            usleep(100000);
-        }
+void threadB() {
+  for (int i = 10; i > 0;) {
+    boost::mutex::scoped_lock lock(mutex, boost::try_to_lock);
+    if (lock) {
+      fprintf(stderr, "Locked by B\n");
+      usleep(100000);
+      fprintf(stderr, "Unlocked by B\n");
+      i--;
+    } else {
+      fprintf(stderr, "Lock failed in B\n");
+      usleep(100000);
     }
+  }
 }
 
-int main(int argc, char **argv)
-{
-    boost::thread a(threadA);
-    boost::thread b(threadB);
-    a.join();
-    b.join();
+int main(int argc, char **argv) {
+  boost::thread a(threadA);
+  boost::thread b(threadB);
+  a.join();
+  b.join();
 }
