27d07c2710987d30f5363bfb3d8321da355eb2ab
WangChenglin <wangchenglin.clark@bytedance.com>
2021-07-02 20:59:14 +0800

    add test cases

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY:all
 
-all:cc_snippets_test,run
+all:cc_snippets_test run
 
 rocksdb:
 	@cd third_party/rocksdb && make -j 8 static_lib
@@ -14,16 +14,12 @@ dependency: rocksdb brpc
 gen_proto:
 	@cd cxx && cp -ur ../proto . && cd proto && protoc *.proto --proto_path=. --cpp_out=.
 
-cc_snippets_test:
-	@rm -f cc_snippets_test
+cc_snippets_test: clear
 	@cd cxx && mkdir -p build && cd build && cmake .. && make -j 8
 	@ln -sf cxx/build/cc_snippets_test cc_snippets_test
 
-cc-lint:
-	@git ls-files | egrep '.*\.(h|cc)' | grep -v trash | xargs cpplint --filter=-legal/copyright,-build/c++11,-build/header_guard
-
-cc-format:
-	@git ls-files | egrep '.*\.(h|cc)' | grep -v trash | xargs clang-format -style=google -i
+clear:
+	@rm -f cc_snippets_test
 
 run:
 	./cc_snippets_test
--- cxx/test_brpc.cc
+++ cxx/test_brpc.cc
@@ -2,6 +2,7 @@
 #include <brpc/server.h>
 #include <unistd.h>
 
+#include <condition_variable>
 #include <thread>
 
 #include "common/common.h"
@@ -32,10 +33,10 @@ class EchoServiceImp : public proto::EchoService {
     brpc::Controller *b_controller =
         static_cast<brpc::Controller *>(controller);
     auto remote = b_controller->remote_side();
-    LOG(INFO) << "request " << request->ShortDebugString()
-              << " received from client"
-              << std::string(butil::ip2str(remote.ip).c_str()) << ":"
-              << remote.port;
+    VLOG(10) << "request " << request->ShortDebugString()
+             << " received from client"
+             << std::string(butil::ip2str(remote.ip).c_str()) << ":"
+             << remote.port;
     std::string resp_msg = "response from " + std::string(butil::my_ip_cstr());
     response->set_message(resp_msg.c_str());
   }
@@ -52,23 +53,63 @@ TEST(Brpc, TestServer) {
 
   ASSERT_EQ(server.Start(43210, &options), 0);
 
-  auto pid = fork();
-
   std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
   LOG(INFO) << "server done";
 }
 
+class BrpcClosure : public ::google::protobuf::Closure {
+ public:
+  static BrpcClosure *New(google::protobuf::Closure *closure) {
+    return new BrpcClosure(closure);
+  }
+
+  void Run() override {
+    closure_->Run();
+    std::unique_lock<std::mutex> ul(mu_);
+    done_ = true;
+    cv_.notify_all();
+  }
+
+  void Wait() {
+    std::unique_lock<std::mutex> ul(mu_);
+    if (!done_) {
+      cv_.wait(ul);
+    }
+    std::unique_ptr<BrpcClosure>(this);
+  }
+
+ private:
+  explicit BrpcClosure(google::protobuf::Closure *closure)
+      : closure_(closure) {}
+
+  std::mutex mu_;
+  std::condition_variable cv_;
+  bool done_{false};
+
+  google::protobuf::Closure *closure_;
+};
+
 TEST(Brpc, TestClient) {
-  brpc::Channel chan;
+  brpc::Server server;
+  EchoServiceImp service;
+  std::atomic_bool started{false};
+  auto server_thread = std::thread([&]() {
+    ASSERT_EQ(server.AddService(&service, brpc::SERVER_DOESNT_OWN_SERVICE), 0);
+    brpc::ServerOptions options;
+    ASSERT_EQ(server.Start(43210, &options), 0);
+    started.store(true);
+    server.Join();
+  });
+  while (!started.load()) {
+  }
 
   brpc::ChannelOptions options;
   options.timeout_ms = 100;
   options.max_retry = 3;
   options.protocol = "baidu_std";
-
+  brpc::Channel chan;
   ASSERT_EQ(chan.Init("0.0.0.0:43210", &options), 0);
-
   proto::EchoService_Stub echo_stub(&chan);
 
   auto response = new proto::EchoResponse();
@@ -76,8 +117,121 @@ TEST(Brpc, TestClient) {
   proto::EchoRequest request;
   request.set_message("hello");
 
-  auto done = brpc::NewCallback(&HandleResponse, cntl, response);
+  auto done =
+      BrpcClosure::New(brpc::NewCallback(&HandleResponse, cntl, response));
   echo_stub.Echo(cntl, &request, response, done);
+  done->Wait();
+
+  ASSERT_EQ(server.Stop(0), 0);
+  if (server_thread.joinable()) {
+    server_thread.join();
+  }
+}
+
+class TestBrpc : public testing::Test {
+ public:
+  void SetUp() {
+    server_ = std::make_unique<brpc::Server>();
+    service_ = std::make_unique<EchoServiceImp>();
+    CHECK_EQ(
+        server_->AddService(service_.get(), brpc::SERVER_DOESNT_OWN_SERVICE),
+        0);
+    server_opts_.num_threads = 0;
+    server_opts_.max_concurrency = 0;
+    CHECK_EQ(server_->Start(port_, &server_opts_), 0);
+  }
+
+  void TearDown() {}
+
+  std::shared_ptr<proto::EchoService_Stub> GetOrCreateStub() {
+    std::lock_guard<std::mutex> guard(mtx_);
+    if (channel_ == nullptr) {
+      channel_ = std::make_unique<brpc::Channel>();
+      channel_opts_.timeout_ms = 30;
+      channel_opts_.max_retry = 3;
+      channel_opts_.protocol = "baidu_std";
+      CHECK_EQ(channel_->Init(server_ip_port_.c_str(), &channel_opts_), 0);
+    }
+    return std::make_shared<proto::EchoService_Stub>(channel_.get());
+  }
+
+ protected:
+  std::unique_ptr<brpc::Server> server_{nullptr};
+  std::unique_ptr<proto::EchoService> service_{nullptr};
+  brpc::ServerOptions server_opts_;
+
+  std::mutex mtx_;
+  std::unique_ptr<brpc::Channel> channel_{nullptr};
+  brpc::ChannelOptions channel_opts_;
+
+  std::string server_ip_port_ = "0.0.0.0:43210";
+  int port_ = 43210;
+};
+
+TEST_F(TestBrpc, Hello) {
+  auto stub = GetOrCreateStub();
+  for (auto i = 0; i < 10; ++i) {
+    proto::EchoRequest request;
+    request.set_message("hello, I'm from client");
+    auto cntl = new brpc::Controller();
+    auto resp = new proto::EchoResponse();
+    auto done = brpc::NewCallback(&HandleResponse, cntl, resp);
+    stub->Echo(cntl, &request, resp, done);
+  }
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+}
+
+class TestBrpc : public testing::Test {
+ public:
+  void SetUp() {
+    server_ = std::make_unique<brpc::Server>();
+    service_ = std::make_unique<EchoServiceImp>();
+    CHECK_EQ(
+        server_->AddService(service_.get(), brpc::SERVER_DOESNT_OWN_SERVICE),
+        0);
+    server_opts_.num_threads = 0;
+    server_opts_.max_concurrency = 0;
+    CHECK_EQ(server_->Start(port_, &server_opts_), 0);
+  }
+
+  void TearDown() {}
+
+  std::shared_ptr<proto::EchoService_Stub> GetOrCreateStub() {
+    std::lock_guard<std::mutex> guard(mtx_);
+    if (channel_ == nullptr) {
+      channel_ = std::make_unique<brpc::Channel>();
+      channel_opts_.timeout_ms = 30;
+      channel_opts_.max_retry = 3;
+      channel_opts_.protocol = "baidu_std";
+      CHECK_EQ(channel_->Init(server_ip_port_.c_str(), &channel_opts_), 0);
+    }
+    return std::make_shared<proto::EchoService_Stub>(channel_.get());
+  }
+
+ protected:
+  std::unique_ptr<brpc::Server> server_{nullptr};
+  std::unique_ptr<proto::EchoService> service_{nullptr};
+  brpc::ServerOptions server_opts_;
+
+  std::mutex mtx_;
+  std::unique_ptr<brpc::Channel> channel_{nullptr};
+  brpc::ChannelOptions channel_opts_;
+
+  std::string server_ip_port_ = "0.0.0.0:43210";
+  int port_ = 43210;
+};
+
+TEST_F(TestBrpc, Hello) {
+  auto stub = GetOrCreateStub();
+  for (auto i = 0; i < 10; ++i) {
+    proto::EchoRequest request;
+    request.set_message("hello, I'm from client");
+    auto cntl = new brpc::Controller();
+    auto resp = new proto::EchoResponse();
+    auto done = brpc::NewCallback(&HandleResponse, cntl, resp);
+    stub->Echo(cntl, &request, resp, done);
+  }
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
 
 }  // namespace snippets
